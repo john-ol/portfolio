5cc7745cbc81e8e170e0909de8d07612b5bc23ac
John <oleinikov.dev@gmail.com>
2021-07-07 17:22:29 +0300

    fix variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
           'style-src': `'self' 'unsafe-inline'`,
           'img-src': `'self' data: https://www.gstatic.com`,
           'font-src': `'self' data: fonts.gstatic.com`,
-          'connect-src': `${GATSBY_API_URL}`,
+          'connect-src': `${process.env.GATSBY_API_URL}`,
         },
       },
     },
